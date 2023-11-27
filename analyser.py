3a3ffb176ba8e17dce288ca0a59d22b34fba2e12
Andre <75341645+andrembarreto@users.noreply.github.com>
2023-11-26 23:53:51 -0300

    added geo db integration

--- analyser.py
+++ analyser.py
@@ -1,22 +1,24 @@
 import asyncio
-import json
 from flask import Flask, request, jsonify
+from db import MobilityDataBaseController
 
 app = Flask(__name__)
 
+db_controller = MobilityDataBaseController()
+
 @app.route("/receive-mobility-data", methods=["POST"])
 async def receive_json():
-    # Wait for the JSON data to be received
     try:
         data = request.json
     except Exception as e:
         error_message = {"message": repr(e)}
         return jsonify(error_message)
 
-    # Send a confirmation response
-    print(data)
+    db_controller.store_journey_entries(data)
+
     confirmation = {"message": "Mobility data received successfully"}
     return jsonify(confirmation)
+
 
 if __name__ == "__main__":
     asyncio.run(app.run(debug=True))
---
+++ db.py
@@ -0,0 +1,55 @@
+import psycopg2
+from datetime import datetime
+import json
+
+class MobilityDataBaseController:
+    def __init__(self):
+        db_info = self.get_db_info()
+        self.connection = psycopg2.connect(database=db_info['database'],
+                                            user=db_info['user'],
+                                            host=db_info['host'],
+                                            password=db_info['password'],
+                                            port=db_info['port'])
+        
+        self.cursor = self.connection.cursor()
+    
+    def store_journey_entries(self, mobility_data: list[dict]) -> bool:
+        self.register_journey_bus_line(mobility_data[0]['bus_line'])
+
+        journey_id = self.fetch_journey_id()
+
+        for journey_point in mobility_data:
+            journey_point['timestamp'] = datetime.strptime(journey_point['timestamp'], "%a %b %d %H:%M:%S %Y %Z")
+            journey_point['journey_id'] = journey_id
+
+            self.cursor.execute('''INSERT INTO mobilidade_schema.jornada(
+                                        aceleracao_x, aceleracao_y, aceleracao_z,
+                                        rotacao_x, rotacao_y, rotacao_z, orientacao,
+                                        id_jornada, timestamp, latitude, longitude)
+                                    VALUES
+                                        (%(acceleration_x)s,%(acceleration_y)s,%(acceleration_z)s,
+                                        %(rotation_x)s,%(rotation_y)s,%(rotation_z)s,
+                                        %(device_orientation)s,%(journey_id)s,%(timestamp)s,
+                                        %(latitude)s,%(longitude)s)''', journey_point)
+        self.connection.commit()
+        self.connection.close()
+
+    def register_journey_bus_line(self, line_id: str):
+        self.cursor.execute(f'INSERT INTO mobilidade_schema.jornada_linha(id_linha) VALUES({line_id})')
+        self.connection.commit()
+
+    def fetch_journey_id(self):
+        self.cursor.execute('SELECT max(id_jornada) FROM mobilidade_schema.jornada_linha')
+        result = self.cursor.fetchall()
+        self.connection.commit()
+
+        if result is None:
+            raise Exception("Journey Id not Found")
+        
+        return result[0][0]
+    
+    def get_db_info(self) -> dict:
+        with open('db_info.json', "r") as db_info_file:
+            db_info = json.loads(db_info_file.read())
+        db_info_file.close()
+        return db_info
