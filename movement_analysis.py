43e42caf8574ee1b601cd3b745bd0a90a2a17a53
Andre <75341645+andrembarreto@users.noreply.github.com>
2024-04-02 10:06:36 -0300

    movement_analysis: refactoring and added stability

--- movement_analysis.py
+++ movement_analysis.py
@@ -2,25 +2,50 @@ import pandas as pd
 import numpy as np
 from enum import Enum
 
+
 class MovementIntensity(Enum):
     LOW_INTENSITY = 0
     MODERATE_INTENSITY = 1
     HIGH_INTENSITY = 2
     VERY_HIGH_INTENSITY = 3
 
-class MovementAnalyser():
-    def __init__(self, point_collection: pd.DataFrame):
-        self.point_collection = point_collection
+
+class MovementStability(Enum):
+    STABLE = 0
+    LIGHT_VIBRATION = 1
+    MODERATE_VIBRATION = 2
+    INTENSE_VIBRATION = 3
+    UNSTABLE = 4
+
+
+class MovementAnalyser:
+    def __init__(self, points: pd.DataFrame):
+        self.points = points
         
     @staticmethod
-    def create_total_pointwise_acceleration_column(df: pd.DataFrame):
+    def create_total_acceleration_column(df: pd.DataFrame) -> None:
         x, y, z = df['acceleration_x'], df['acceleration_y'], df['acceleration_z']
         result = np.hypot(x, y, z)
         df['total_acceleration'] = result
 
     @staticmethod
+    def create_segment_rotation_variation_column(segment: pd.Series):
+        pass
+
+    @staticmethod
     def get_mean_total_acceleration(df: pd.DataFrame) -> float:
         return df['total_acceleration'].mean()
+
+    @staticmethod
+    def get_mean_total_rotation_variation(df: pd.DataFrame) -> float:
+        return df['rotation_variation'].mean()
+
+
+    # pegar valores de rotacao em cada eixo dos dois pontos que definem um trecho
+    @staticmethod
+    def get_segment_points_rotation_values(df: pd.DataFrame, point_id_1: int, point_id_2: int) -> list[pd.Series]:
+        point_1_rotation_values = df[point_id_1, ['']]
+        ...
 
     @staticmethod
     def classify_by_relative_intensity(df: pd.DataFrame, value_index: int) -> MovementIntensity:
@@ -30,12 +55,26 @@ class MovementAnalyser():
 
         if relative_intensity < 0.2:
             return MovementIntensity.LOW_INTENSITY
-        if 0.2 <= relative_intensity and relative_intensity < 1.2:
+        elif relative_intensity < 1.2:
             return MovementIntensity.MODERATE_INTENSITY
-        if 1.2 <= relative_intensity and relative_intensity < 2.0:
+        elif relative_intensity < 2.0:
             return MovementIntensity.HIGH_INTENSITY
-        if relative_intensity > 2.0:
+        else:
             return MovementIntensity.VERY_HIGH_INTENSITY
-        
 
-    
+    @staticmethod
+    def classify_by_relative_stability(df: pd.DataFrame, value_index: int) -> MovementStability:
+        mean_rotation_variation = MovementAnalyser.get_mean_total_rotation_variation(df)
+
+        relative_variation = df.loc[value_index, 'rotation_variation'] / mean_rotation_variation
+
+        if relative_variation < 0.01:
+            return MovementStability.STABLE
+        if relative_variation < 0.2:
+            return MovementStability.LIGHT_VIBRATION
+        elif relative_variation < 1.2:
+            return MovementStability.MODERATE_VIBRATION
+        elif relative_variation < 2.0:
+            return MovementStability.INTENSE_VIBRATION
+        else:
+            return MovementStability.UNSTABLE
--- test_movement_analysis.py
+++ test_movement_analysis.py
@@ -2,63 +2,71 @@ import pytest
 from movement_analysis import *
 import statistics
 
+
 @pytest.fixture
 def movement_intensity_dataframe_with_mean_1() -> pd.DataFrame:
-    acceleration_values_in_x_axis = [1.0] * 1000 # 1000 elements to make the mean close enough to 1 when a new (regular) value is added
+    # 1000 elements to make the mean close enough to 1 when a new (regular) value is added
+    acceleration_values_in_x_axis = [1.0] * 1000
     df = pd.DataFrame({
-        'acceleration_x' : acceleration_values_in_x_axis,
-        'acceleration_y' : [0.0] * len(acceleration_values_in_x_axis),
-        'acceleration_z' : [0.0] * len(acceleration_values_in_x_axis)
+        'acceleration_x': acceleration_values_in_x_axis,
+        'acceleration_y': [0.0] * len(acceleration_values_in_x_axis),
+        'acceleration_z': [0.0] * len(acceleration_values_in_x_axis)
     })
     yield df
+
 
 def test_mean_total_acceleration_when_object_is_absolutely_still():
     acceleration_data = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]
     df = pd.DataFrame(acceleration_data, columns=['acceleration_x', 'acceleration_y', 'acceleration_z'])
     
-    MovementAnalyser.create_total_pointwise_acceleration_column(df)
+    MovementAnalyser.create_total_acceleration_column(df)
     mean_total_acceleration = MovementAnalyser.get_mean_total_acceleration(df)
 
     assert(mean_total_acceleration == 0.0)
+
 
 def test_mean_total_acceleration_on_takeoff():
     y_acceleration_values_on_takeoff = [1.5, 2.2, 3.0, 3.8, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0]
     
     df = pd.DataFrame({
-        'acceleration_x' : [0.0] * len(y_acceleration_values_on_takeoff),
-        'acceleration_y' : y_acceleration_values_on_takeoff,
-        'acceleration_z' : [0.0] * len(y_acceleration_values_on_takeoff)
+        'acceleration_x': [0.0] * len(y_acceleration_values_on_takeoff),
+        'acceleration_y': y_acceleration_values_on_takeoff,
+        'acceleration_z': [0.0] * len(y_acceleration_values_on_takeoff)
     })
 
-    MovementAnalyser.create_total_pointwise_acceleration_column(df)
+    MovementAnalyser.create_total_acceleration_column(df)
     mean_total_acceleration = MovementAnalyser.get_mean_total_acceleration(df)
 
     assert(mean_total_acceleration == statistics.mean(y_acceleration_values_on_takeoff))
-    
+
+
 def test_classifying_low_intensity_movement(movement_intensity_dataframe_with_mean_1: pd.DataFrame):
     movement_intensity_dataframe_with_mean_1.loc[0] = {'acceleration_x': 0.15, 'acceleration_y': 0, 'acceleration_z': 0}
-    MovementAnalyser.create_total_pointwise_acceleration_column(movement_intensity_dataframe_with_mean_1)
+    MovementAnalyser.create_total_acceleration_column(movement_intensity_dataframe_with_mean_1)
     
     intensity = MovementAnalyser.classify_by_relative_intensity(movement_intensity_dataframe_with_mean_1, 0)
     assert(intensity == MovementIntensity.LOW_INTENSITY)
 
+
 def test_classifying_moderate_intensity_movement(movement_intensity_dataframe_with_mean_1: pd.DataFrame):
     movement_intensity_dataframe_with_mean_1.loc[0] = {'acceleration_x': 0.5, 'acceleration_y': 0, 'acceleration_z': 0}
-    MovementAnalyser.create_total_pointwise_acceleration_column(movement_intensity_dataframe_with_mean_1)
+    MovementAnalyser.create_total_acceleration_column(movement_intensity_dataframe_with_mean_1)
 
     intensity = MovementAnalyser.classify_by_relative_intensity(movement_intensity_dataframe_with_mean_1, 0)
     assert(intensity == MovementIntensity.MODERATE_INTENSITY)
 
+
 def test_classifying_high_intensity_movement(movement_intensity_dataframe_with_mean_1: pd.DataFrame):
     movement_intensity_dataframe_with_mean_1.loc[0] = {'acceleration_x': 1.5, 'acceleration_y': 0, 'acceleration_z': 0}
-    MovementAnalyser.create_total_pointwise_acceleration_column(movement_intensity_dataframe_with_mean_1)
+    MovementAnalyser.create_total_acceleration_column(movement_intensity_dataframe_with_mean_1)
     
     intensity = MovementAnalyser.classify_by_relative_intensity(movement_intensity_dataframe_with_mean_1, 0)
     assert(intensity == MovementIntensity.HIGH_INTENSITY)
 
+
 def test_classifying_very_high_intensity_movement(movement_intensity_dataframe_with_mean_1: pd.DataFrame):
     movement_intensity_dataframe_with_mean_1.loc[0] = {'acceleration_x': 2.5, 'acceleration_y': 0, 'acceleration_z': 0}
-    MovementAnalyser.create_total_pointwise_acceleration_column(movement_intensity_dataframe_with_mean_1)
+    MovementAnalyser.create_total_acceleration_column(movement_intensity_dataframe_with_mean_1)
     
     intensity = MovementAnalyser.classify_by_relative_intensity(movement_intensity_dataframe_with_mean_1, 0)
     assert(intensity == MovementIntensity.VERY_HIGH_INTENSITY)
