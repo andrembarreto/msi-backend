2a90b1c3c6c3a6489c5d1ada9ed64c4b7b06d1bc
Andre Barreto <andrelmb1509@gmail.com>
2024-06-16 14:42:18 -0300

    added journey registration method declaration

--- mobility_db_controller.py
+++ mobility_db_controller.py
@@ -13,7 +13,7 @@ class MobilityDBController:
         journey_id: int = self.__retrieve_journey_id()
         bus_line: str = journey_data[0]['bus_line']
         self.__register_bus_line(bus_line)
-        # TODO: create a journey_id - bus_line link in the database
+        self.__register_journey(journey_id, bus_line)
 
         for point in journey_data:
             entry = { field: value for field, value in point.items() }
@@ -27,5 +27,13 @@ class MobilityDBController:
         ...
 
     def __register_bus_line(self, bus_line: str) -> None:
-        """ Registers `bus_line` to the bus lines table in the database, if not yet registered """
+        """ Creates a new entry in the bus line table using `bus_line`,
+        if one does not already exist.
+        """
+        ...
+
+    def __register_journey(self, journey_id: int, bus_line: str) -> None:
+        """ Creates a new entry in the journey table using `journey_id`
+        and linking it to a bus line identified by the `bus_line` value
+        """
         ...
