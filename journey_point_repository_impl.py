30c746842ada05bbcfc4909ea25c22c30a1f3de0
Andre Barreto <andrelmb1509@gmail.com>
2024-06-30 15:54:12 -0300

    added implementation for get_points_by_journey_id

--- journey_point_repository_impl.py
+++ journey_point_repository_impl.py
@@ -1,10 +1,13 @@
-from models import JourneyPoint
+from models import JourneyPoint, Journey
 from journey_point_repository import JourneyPointRepository
 
 
 class JourneyPointRepositoryImpl(JourneyPointRepository):
     def get_points_by_journey_id(self, journey_id: int):
-        raise NotImplementedError()
+        query = JourneyPoint.select()\
+                            .join(Journey)\
+                            .where(JourneyPoint.journey == journey_id)
+        return query
 
     def create(self, **kwargs):
         return JourneyPoint.create(**kwargs)
