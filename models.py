625788dd2aac43898ed0eb0429ce43a7c7f76329
Andre <75341645+andrembarreto@users.noreply.github.com>
2024-07-07 19:21:36 -0300

    fix: implemented geometry field's db -> python conversion

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from peewee import Model, CharField, ForeignKeyField, DateTimeField, AutoField, FloatField, fn, Field
 
 from database import db
+from shapely import wkb
 
 
 class BaseModel(Model):
@@ -17,20 +18,20 @@ class Journey(BaseModel):
     bus_line = ForeignKeyField(BusLine, backref='journeys')
 
 
-class PointField(Field):
+class GeometryField(Field):
     field_type = 'geometry'
 
     def db_value(self, value):
         return fn.ST_GeomFromText(*value)
 
     def python_value(self, value):
-        return fn.ST_AsGeoJson(value)
+        return wkb.loads(value)
 
 
 class JourneyPoint(BaseModel):
     id = AutoField()
     journey = ForeignKeyField(Journey, backref='journey_points')
-    geometry = PointField()
+    geometry = GeometryField()
     timestamp = DateTimeField()
     acceleration_x = FloatField()
     acceleration_y = FloatField()
